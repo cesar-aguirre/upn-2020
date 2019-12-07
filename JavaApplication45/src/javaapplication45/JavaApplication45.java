a5f4f46b8b6063ccef0a1d735fcb59573aca5d22
Estudiante <Estudiante@ULNLAB1441>
2019-12-07 12:02:27 -0500

    Imprimiendo mensaje de año nuevo

---
+++ JavaApplication45/src/javaapplication45/JavaApplication45.java
@@ -0,0 +1,9 @@
+package javaapplication45;
+
+public class JavaApplication45 {
+
+    public static void main(String[] args) {
+        System.out.println("Feliz año nuevo 2020");    
+    }
+    
+}
